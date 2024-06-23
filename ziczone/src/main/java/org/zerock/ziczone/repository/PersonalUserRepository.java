7ae7c5a26e5c11f62d1c33dfdbd444532ebd8329
minjaejeo <157557014+minjaejeo@users.noreply.github.com>
2024-06-24 00:14:35 +0900

    companyUser, PickAndScrap, resume, portfolio, certificate domain까지 생성 test까지 완료

--- ziczone/src/main/java/org/zerock/ziczone/repository/PersonalUserRepository.java
+++ ziczone/src/main/java/org/zerock/ziczone/repository/PersonalUserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.ziczone.domain.PersonalUser;
 
 public interface PersonalUserRepository extends JpaRepository<PersonalUser, Long> {
+    PersonalUser findByPersonalId(Long personalId);
 }
--- ziczone/src/test/java/org/zerock/ziczone/repository/UserRepositoryTests.java
+++ ziczone/src/test/java/org/zerock/ziczone/repository/UserRepositoryTests.java
@@ -4,10 +4,9 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.zerock.ziczone.domain.Gender;
-import org.zerock.ziczone.domain.PersonalUser;
-import org.zerock.ziczone.domain.User;
-import org.zerock.ziczone.domain.UserType;
+import org.zerock.ziczone.domain.*;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 @Log4j2
@@ -17,9 +16,19 @@ public class UserRepositoryTests {
     private UserRepository userRepository;
     @Autowired
     private PersonalUserRepository personalUserRepository;
+    @Autowired
+    private CompanyUserRepository companyUserRepository;
+    @Autowired
+    private PickAndScrapRepository pickAndScrapRepository;
+    @Autowired
+    private ResumeRepository resumeRepository;
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+    @Autowired
+    private CertificateRepository certificateRepository;
 
     @Test
-    public void testInsert(){
+    public void testPersonalInsert(){
         User user = User.builder()
                 .userName("전민재")
                 .email("alswo9672@gmail.com")
@@ -40,5 +49,89 @@ public class UserRepositoryTests {
 
         log.info("User saved: " + user);
         log.info("PersonalUser saved: " + personalUser);
+    }
+
+    @Test
+    public void testCompanyInsert(){
+        User user = User.builder()
+                .userName("토스")
+                .email("support@toss.im")
+                .password("1234")
+                .userIntro("toss입니다.")
+                .userType(UserType.COMPANY)
+                .build();
+        userRepository.save(user);
+
+        LocalDate companyYear = LocalDate.of(2013,8,1);
+
+        CompanyUser companyUser = CompanyUser.builder()
+                .companyNum("226-27-20508")
+                .companyAddr("서울특별시 강남구 테헤란로 131")
+                .companyYear(companyYear)
+                .companyLogo("http://toss.png")
+                .companyCeo("이승건")
+                .user(user)
+                .build();
+        companyUserRepository.save(companyUser);
+
+        log.info("User saved: " + user);
+        log.info("CompanyUser saved: " + companyUser);
+    }
+
+    @Test
+    public void testPickAndScrapInsert(){
+        CompanyUser companyUser = companyUserRepository.findByCompanyId(1L);
+        PersonalUser personalUser = personalUserRepository.findByPersonalId(1L);
+        PickAndScrap pickAndScrap = PickAndScrap.builder()
+                .pick(true)
+                .scrap(true)
+                .companyUser(companyUser)
+                .personalUser(personalUser)
+                .build();
+        pickAndScrapRepository.save(pickAndScrap);
+
+        log.info("PickAndScrap saved: " + pickAndScrap);
+    }
+    @Test
+    public void testResumeInsert(){
+        PersonalUser personalUser = personalUserRepository.findByPersonalId(1L);
+
+        Resume resume = Resume.builder()
+                .resumeName("전민재")
+                .date("1998년04월06일")
+                .phoneNum("010-2427-9672")
+                .resumePhoto("http://photo.png")
+                .personalState("http://personalState.png")
+                .personalUser(personalUser)
+                .build();
+
+        resumeRepository.save(resume);
+        log.info("Resume saved: " + resume);
+    }
+    @Test
+    public void testPortfolioInsert(){
+        Resume resume = resumeRepository.findByResumeId(1L);
+
+        Portfolio portfolio = Portfolio.builder()
+                .portFile("http://portfoliofile.png")
+                .resume(resume)
+                .build();
+        portfolioRepository.save(portfolio);
+        log.info("Portfolio saved: " + portfolio);
+
+    }
+    @Test
+    public void testCertificateInsert(){
+        Resume resume = resumeRepository.findByResumeId(1L);
+
+        Certificate certificate = Certificate.builder()
+                .cert("정보처리기사")
+                .certDate("2024-06-23")
+                .resume(resume)
+                .build();
+        certificateRepository.save(certificate);
+
+        log.info("Certificate saved: " + certificate);
+
     }
 }
