51850b8e66e54bd7ef15e1d06930866e3128694f
CHAERIM LEE <leechaerim214@gmail.com>
2024-07-08 00:46:26 +0900

    feat: NCP 서비스 사용을 위한 설정, CORS 설정

---
+++ ziczone/src/main/java/org/zerock/ziczone/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package org.zerock.ziczone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000") // 요청을 허용할 도메인
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
