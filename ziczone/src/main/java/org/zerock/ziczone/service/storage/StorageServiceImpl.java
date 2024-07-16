1dc20b7eed91960e63a07d7a06b369f4265fd760
KangHyun_Kim <162311939+hmmumm@users.noreply.github.com>
2024-07-16 12:00:24 +0900

    오브젝트 스토리지 파일 컨텐트 타입별로 등록 추가

--- ziczone/src/main/java/org/zerock/ziczone/service/storage/StorageServiceImpl.java
+++ ziczone/src/main/java/org/zerock/ziczone/service/storage/StorageServiceImpl.java
@@ -32,13 +32,16 @@ public class StorageServiceImpl implements StorageService {
         String fileUrl = null;
         try {
             ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
             metadata.setContentLength(file.getSize());
             amazonS3.putObject(new PutObjectRequest(bucketName, objectName, file.getInputStream(), metadata));
             // 업로드된 파일의 접근 제어 리스트 가져오기
             AccessControlList accessControlList = amazonS3.getObjectAcl(bucketName, objectName);
             //  모든 사용자에게 읽기 권한 부여
             accessControlList.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+
             amazonS3.setObjectAcl(bucketName, objectName, accessControlList);
+
             // 업로드된 파일의 URL 가져오기
             fileUrl = amazonS3.getUrl(bucketName, objectName).toString();
 
